da9daf2c0c4c4f9d82d2dc639cfa247617a317a6
Janntul Afroz Faria <afroz_faria03@yahoo.com>
2024-09-08 03:30:48 +0600

    ul replaced with navlink variable

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -28,11 +28,7 @@ const NavBar = () => {
             <nav className="bg-sky-600 p-6 flex justify-between">
                 <h6 className="text-3xl text-sky-200"> <span className="text-purple-200">Next</span> Practice</h6>
                 <ul className="flex gap-4 text-white">
-                    <Link href={'/about'} >About</Link>
-                    <Link href={'/service'} >Service</Link>
-                    <Link href={'/portfolio'} >Port Folio</Link>
-                    <Link href={'/blogs'} >Blogs</Link>
-                    <Link href={'/contact'} >Contact</Link>
+                    {links}
                 </ul>
             </nav>
         </div>
