bf41b47b7557e1319ee0bd2ff9f5dc9179f37f18
Janntul Afroz Faria <afroz_faria03@yahoo.com>
2024-09-08 03:27:06 +0600

    navbar style modified

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,10 +1,11 @@
+import Link from "next/link";
 
 const NavBar = () => {
     return (
         <div>
-            <nav className="bg-sky-300 p-6 flex justify-between">
-                <h6 className="text-3xl"> <span className="text-purple-500">Next</span> Practice</h6>
-                <ul>
+            <nav className="bg-sky-600 p-6 flex justify-between">
+                <h6 className="text-3xl text-sky-200"> <span className="text-purple-200">Next</span> Practice</h6>
+                <ul className="flex gap-4 text-white">
                     <Link href={'/about'} >About</Link>
                     <Link href={'/service'} >Service</Link>
                     <Link href={'/portfolio'} >Port Folio</Link>
