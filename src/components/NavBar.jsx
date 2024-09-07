11a764696cf734a5573a8c41ec7ab5ef1b3452fe
Janntul Afroz Faria <afroz_faria03@yahoo.com>
2024-09-08 03:19:19 +0600

    website name included in navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -3,7 +3,7 @@ const NavBar = () => {
     return (
         <div>
             <nav className="bg-sky-300 p-6 flex justify-between">
-                <h6>Logo</h6>
+                <h6> <span className="text-purple-500">Next</span> Practice</h6>
                 <ul>
                     <Link href={'/about'} >About</Link>
                     <Link href={'/service'} >Service</Link>
