61581c83bdbf1a25493cf38693490f5c077623bf
Janntul Afroz Faria <afroz_faria03@yahoo.com>
2024-09-08 03:09:34 +0600

    contents added in blogs page

--- src/app/blogs/page.jsx
+++ src/app/blogs/page.jsx
@@ -2,7 +2,7 @@
 const BlogsPage = () => {
     return (
         <div>
-            
+            <h1>This is the Blogs section where you can go through blogs written by our contributors</h1>
         </div>
     );
 };
