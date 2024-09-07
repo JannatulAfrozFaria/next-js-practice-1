ac643224e8aa59f376d5f6cdd2c3c5620a36398a
Janntul Afroz Faria <afroz_faria03@yahoo.com>
2024-09-08 03:05:18 +0600

    portfolio page set up complete

---
+++ src/app/portfolio/page.jsx
@@ -0,0 +1,11 @@
+
+
+const portFolio = () => {
+    return (
+        <div>
+            <h3>We have various projects to display in this section</h3>
+        </div>
+    );
+};
+
+export default portFolio;
