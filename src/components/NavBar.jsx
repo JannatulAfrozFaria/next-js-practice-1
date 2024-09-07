75bb4bd93757f319f45fbdb46f4e2ad0d5a09a59
Janntul Afroz Faria <afroz_faria03@yahoo.com>
2024-09-08 03:18:02 +0600

    navbar style modified

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -2,7 +2,8 @@
 const NavBar = () => {
     return (
         <div>
-            <nav className="bg-sky-300 p-6">
+            <nav className="bg-sky-300 p-6 flex justify-between">
+                <h6>Logo</h6>
                 <ul>
                     <Link href={'/about'} >About</Link>
                     <Link href={'/service'} >Service</Link>
