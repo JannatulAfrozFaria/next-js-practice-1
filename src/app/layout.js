3131eedfa7db825d9247c90d954d3ab7cc92f6f4
Janntul Afroz Faria <afroz_faria03@yahoo.com>
2024-09-08 03:13:11 +0600

    navbar removed from layout

--- src/app/layout.js
+++ src/app/layout.js
@@ -24,16 +24,7 @@ export default function RootLayout({ children }) {
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <nav className="bg-sky-300 p-6">
-          <h6>Logo</h6>
-          <ul>
-            <Link href={'/about'} >About</Link>
-            <Link href={'/service'} >Service</Link>
-            <Link href={'/portfolio'} >Port Folio</Link>
-            <Link href={'/blogs'} >Blogs</Link>
-            <Link href={'/contact'} >Contact</Link>
-          </ul>
-        </nav>
+        
        <div className="h-screen">
             {children}
        </div>
