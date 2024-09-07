9138c9912da3b0d1237914c570c39247e11ade83
Janntul Afroz Faria <afroz_faria03@yahoo.com>
2024-09-08 03:14:01 +0600

    navbar moved to particular content

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -2,7 +2,16 @@
 const NavBar = () => {
     return (
         <div>
-            
+            <nav className="bg-sky-300 p-6">
+                <h6>Logo</h6>
+                <ul>
+                    <Link href={'/about'} >About</Link>
+                    <Link href={'/service'} >Service</Link>
+                    <Link href={'/portfolio'} >Port Folio</Link>
+                    <Link href={'/blogs'} >Blogs</Link>
+                    <Link href={'/contact'} >Contact</Link>
+                </ul>
+            </nav>
         </div>
     );
 };
