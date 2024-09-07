1ae61e0ed705ac6449e1810c19bff3e9124db668
Janntul Afroz Faria <afroz_faria03@yahoo.com>
2024-09-08 03:34:01 +0600

    Link added in navbar details

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -28,6 +28,9 @@ const NavBar = () => {
             <nav className="bg-sky-600 p-6 flex justify-between">
                 <h6 className="text-3xl text-sky-200"> <span className="text-purple-200">Next</span> Practice</h6>
                 <ul className="flex gap-4 text-white">
+                    {
+                        links?.map((link)=> <Link></Link> )
+                    }
                     {links}
                 </ul>
             </nav>
