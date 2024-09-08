628380390541739a8d9da960af2a66873f054ac3
Janntul Afroz Faria <afroz_faria03@yahoo.com>
2024-09-08 16:49:19 +0600

    children, header , footer composed in dashboard layout

--- src/app/(dashboard)/dashboard/layout.jsx
+++ src/app/(dashboard)/dashboard/layout.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
 
-const DashboardLayout = () => {
+const DashboardLayout = ({children}) => {
     return (
         <div>
-            
+            <div className='bg-blue-300 p-6'>
+                Dashboard Layout
+            </div>
+            {children}
+            <footer>
+                Dashboard Footer
+            </footer>
         </div>
     );
 };
