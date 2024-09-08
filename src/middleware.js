f5686dde0b4c116f80c1d643e3c7d40bcd8ebb32
Janntul Afroz Faria <afroz_faria03@yahoo.com>
2024-09-09 03:36:45 +0600

    about page redirected to about/history

--- src/middleware.js
+++ src/middleware.js
@@ -1,8 +1,8 @@
 import { NextResponse } from "next/server"
 
 export const middleware = (request) =>{
-    return NextResponse.redirect(new URL('/about',request.url))
+    return NextResponse.redirect(new URL('/about/history',request.url))
 }
 export const config = {
-    matcher: '/user'
+    matcher: '/about'
 }
