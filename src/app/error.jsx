e883a74f8ed8ffb0c9b7931d9a1b45299fdbe45d
Janntul Afroz Faria <afroz_faria03@yahoo.com>
2024-09-09 02:22:53 +0600

    error page content added

--- src/app/error.jsx
+++ src/app/error.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const error = () => {
     return (
         <div>
-            
+            <h1 className='text-red-600'>Common Error Display Page </h1>
         </div>
     );
 };
