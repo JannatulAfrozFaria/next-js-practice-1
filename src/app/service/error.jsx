9399fd0b97065d87a9118ff5c0f3fc7dd12e6832
Janntul Afroz Faria <afroz_faria03@yahoo.com>
2024-09-09 02:31:50 +0600

    service error content modified

--- src/app/service/error.jsx
+++ src/app/service/error.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const error = () => {
     return (
         <div>
-            <h1>Service Error</h1>
+            <h1>Found Service Error</h1>
         </div>
     );
 };
