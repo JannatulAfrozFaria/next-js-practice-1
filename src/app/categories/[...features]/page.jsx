86765fa5a9362e38755dadffad117fc8ca671cee
Janntul Afroz Faria <afroz_faria03@yahoo.com>
2024-09-08 18:10:14 +0600

    content added in categories details page

--- src/app/categories/[...features]/page.jsx
+++ src/app/categories/[...features]/page.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 
-const page = () => {
+const CategoriesDetailsPage = () => {
     return (
         <div>
-            
+            <h1>Categories Details Page</h1>
         </div>
     );
 };
 
-export default page;
+export default CategoriesDetailsPage;
