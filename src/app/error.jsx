10ad0ce4acd7ca259fc54a8a3ad16948ce78beb7
Janntul Afroz Faria <afroz_faria03@yahoo.com>
2024-09-09 02:23:23 +0600

    error content style updated

--- src/app/error.jsx
+++ src/app/error.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const error = () => {
     return (
         <div>
-            <h1 className='text-red-600'>Common Error Display Page </h1>
+            <h1 className='text-red-600 text-2xl'>Common Error Display Page </h1>
         </div>
     );
 };
