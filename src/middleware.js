961e3b0e0514a5497e0c4428f8e1f6a83cdec60b
Janntul Afroz Faria <afroz_faria03@yahoo.com>
2024-09-09 03:40:09 +0600

    rewrite introduced

--- src/middleware.js
+++ src/middleware.js
@@ -2,6 +2,8 @@ import { NextResponse } from "next/server"
 
 export const middleware = (request) =>{
     return NextResponse.redirect(new URL('/about/history',request.url))
+    //redirect changes the url and content
+    //rewrite changes the content but does not change the url path name
 }
 export const config = {
     matcher: '/about'
