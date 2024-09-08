ccb98620d7194da2a1b8c15c33c6db873ad3efab
Janntul Afroz Faria <afroz_faria03@yahoo.com>
2024-09-09 02:09:14 +0600

    multiple params implemented in categories

--- src/app/categories/[...features]/page.jsx
+++ src/app/categories/[...features]/page.jsx
@@ -1,7 +1,10 @@
 import React from 'react';
 
 const CategoriesDetailsPage = ({params}) => {
-    console.log(params);
+    if(params.features.length == 3)
+        return <div>{params.features[2]} </div>
+    if(params.features.length==2)
+        return <div>{params.features[1]} </div>
     return (
         <div className='h-screen'>
             <h1>Categories Details Page</h1>
