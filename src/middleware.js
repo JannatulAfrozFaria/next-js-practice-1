f3fe494800ca6f05aeb49711a10f9c824dac6e98
Janntul Afroz Faria <afroz_faria03@yahoo.com>
2024-09-09 02:44:33 +0600

    middleware exported from js file

--- src/middleware.js
+++ src/middleware.js
@@ -1,11 +1,3 @@
-import React from 'react';
-
-const middleware = () => {
-    return (
-        <div>
-            
-        </div>
-    );
-};
-
-export default middleware;
+export const middleware = () =>{
+    
+}
