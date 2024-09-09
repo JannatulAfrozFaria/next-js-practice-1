9979b88d84e61371cdce771ec781b65d2450ce57
Janntul Afroz Faria <afroz_faria03@yahoo.com>
2024-09-10 01:14:13 +0600

    border added in form

--- src/app/about/login/page.jsx
+++ src/app/about/login/page.jsx
@@ -2,14 +2,14 @@ import React from 'react';
 
 const page = () => {
     return (
-        <div className=' p-8 text-center'>
+        <div className=' p-8 text-center border-2 border-blue-400'>
             <h1>Login form</h1>
             <h2>Email: </h2>
             <input type="email" name="Your Email" id="" />
             <h2>Password: </h2>
             <input type="password" name="Your Password" id="" />
             <br />
-            <button className='text- white bg-sky-500 p-2 rounded-lg'>Login</button>
+            <button className=' py-3text-white bg-sky-500 p-2 rounded-lg'>Login</button>
         </div>
     );
 };
