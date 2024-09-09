047b8f325dba1958600bebf50bddf228b561bbc4
Janntul Afroz Faria <afroz_faria03@yahoo.com>
2024-09-10 00:55:45 +0600

    conditional syntax modified

--- src/middleware.js
+++ src/middleware.js
@@ -1,8 +1,10 @@
 import { NextResponse } from "next/server"
 
 export const middleware = (request) =>{
-    if(request.url === '/about')
-    return NextResponse.redirect(new URL('/contact',request.url))
+    if(request.url === '/about'){
+        return NextResponse.redirect(new URL('/contact',request.url))
+    }
+    
     //redirect changes the url and content
     //rewrite changes the content but does not change the url path name
 }
