8675f392b2e759bfb02544097b746c35c7b59cb0
Janntul Afroz Faria <afroz_faria03@yahoo.com>
2024-09-10 00:58:52 +0600

    startsWith() correction done

--- src/middleware.js
+++ src/middleware.js
@@ -1,7 +1,7 @@
 import { NextResponse } from "next/server"
 
 export const middleware = (request) =>{
-    if(request.nextUrl.pathname.startWith('/about')){
+    if(request.nextUrl.pathname.startsWith('/about')){
         return NextResponse.redirect(new URL('/contact',request.url))
     }
     
