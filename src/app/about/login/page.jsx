e0a4b42358f5fa49d60c5fa85f960e079887a9c9
Janntul Afroz Faria <afroz_faria03@yahoo.com>
2024-09-10 01:11:15 +0600

    EMail label added

--- src/app/about/login/page.jsx
+++ src/app/about/login/page.jsx
@@ -4,6 +4,7 @@ const page = () => {
     return (
         <div>
             <h1>Login form</h1>
+            <h2>Email: </h2>
             <input type="email" name="Your Email" id="" />
             <input type="password" name="Your Password" id="" />
             <button className='text- white bg-sky-500 p-2 rounded-lg'>Login</button>
