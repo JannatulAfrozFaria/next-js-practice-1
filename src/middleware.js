1a6a5a93fd8616f812ea3b5d3824b27b58815120
Janntul Afroz Faria <afroz_faria03@yahoo.com>
2024-09-10 00:53:51 +0600

    matcher commented out

--- src/middleware.js
+++ src/middleware.js
@@ -8,5 +8,5 @@ export const middleware = (request) =>{
 export const config = {
     // matcher: '/about'
     //redirect can be used for multiple pages as below with [ ] and comma(,)
-     matcher: ['/about','/service']
+    //  matcher: ['/about','/service']
 }
