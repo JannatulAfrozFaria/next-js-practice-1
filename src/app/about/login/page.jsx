4a74d031bc72642b2306d1f4b57e5e8623ec9861
Janntul Afroz Faria <afroz_faria03@yahoo.com>
2024-09-10 01:09:39 +0600

    inputs added in login form

--- src/app/about/login/page.jsx
+++ src/app/about/login/page.jsx
@@ -3,7 +3,10 @@ import React from 'react';
 const page = () => {
     return (
         <div>
-            
+            <h1>Login form</h1>
+            <input type="email" name="Your Email" id="" />
+            <input type="password" name="Your Password" id="" />
+            <button>Login</button>
         </div>
     );
 };
